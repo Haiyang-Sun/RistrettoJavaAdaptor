7b027855d62e4707c99d7cb84108598499744838
haiyang <sunhaiyangsjtu@gmail.com>
2017-04-12 16:03:09 +0200

    fix java APIs

--- src/ristretto/drip/Drip2Perc.java
+++ src/ristretto/drip/Drip2Perc.java
@@ -1,39 +1,30 @@
 package ristretto.drip;
 
 import ristretto.drip.DripSyntax;
-import ristretto.drip.DripSyntax.Exp;
 import ristretto.perc.PercSyntax;
 
 public class Drip2Perc {
-	public static PercSyntax.CJmp translate(DripSyntax.Exp exp, String label){
+	public static scala.collection.immutable.List<PercSyntax.Stm> cjmp(DripSyntax.Exp exp, String label){
 		return null;
 	}
 
-	public static PercSyntax.Jmp translate(String label){
+	public static scala.collection.immutable.List<PercSyntax.Stm> jmp(String label){
 		return null;
 	}
 	
-	public static PercSyntax.Store translate(int offset, DripSyntax.Exp address, DripSyntax.Exp value){
+	public static scala.collection.immutable.List<PercSyntax.Stm> store(int offset, DripSyntax.Exp address, DripSyntax.Exp value){
 		return null;
 	}
 	
-	public static PercSyntax.Call translate(DripSyntax.Exp address, scala.collection.immutable.List<DripSyntax.Exp> optExps, String t){
-		
+	public static scala.collection.immutable.List<PercSyntax.Stm> call(DripSyntax.Exp address, scala.collection.immutable.List<DripSyntax.Exp> optExps, String t){
 		return null;
 	}
 	
-	public static PercSyntax.Load translate(int offset, DripSyntax.Exp address, String t){
+	public static scala.collection.immutable.List<PercSyntax.Stm> load(int offset, DripSyntax.Exp address, String t){
 		return null;
 	}
 	
-	public static PercSyntax.Bin translate(DripSyntax.Operator operator, DripSyntax.Exp exp11, DripSyntax.Exp exp12, String t){
+	public static scala.collection.immutable.List<PercSyntax.Stm> bin(DripSyntax.Operator operator, DripSyntax.Exp exp11, DripSyntax.Exp exp12, String t){
 		return null;
-	}
-	
-	private void scalaCollectionExample(scala.collection.immutable.List<DripSyntax.Exp> optExps){
-		scala.collection.Iterator<Exp> iter = optExps.iterator();
-		while(iter.hasNext()){
-			Exp e = iter.next();
-		}
 	}
 }
